cec7db05d470c8753174da0443922d991c7f9d72
vivek250391 <65803436+vivek250391@users.noreply.github.com>
2020-05-24 15:56:41 +0530

    nav changes
    nav changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
   {
-    path:'home',component:HomeComponent
+    path:'',component:HomeComponent
 },
 {
   path:'product',component:ProductComponent
