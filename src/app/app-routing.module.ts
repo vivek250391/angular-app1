e2d0bfb1af685136c2f6b9e64c1ca9843ae1e6fe
vivek250391 <65803436+vivek250391@users.noreply.github.com>
2020-05-24 15:08:50 +0530

    app changes
    app chnages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductComponent } from './product/product.component';
+import {HomeComponent} from './home/home.component';
 
 
 const routes: Routes = [
   {
-    path:'',component:ProductComponent
+    path:'',component:HomeComponent
 },
 {
   path:'product',component:ProductComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
